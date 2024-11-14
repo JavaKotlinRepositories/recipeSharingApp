9d8655b2cf141e1dbb91052e4259b4fb9c328dda
VISH01AL <g.vish1al@gmail.com>
2024-11-14 04:22:29 -0500

    started working on login in frontend

--- recipeFrontend/src/app/login/login.component.ts
+++ recipeFrontend/src/app/login/login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  
+  email="";password="";loginServ:LoginService;
+  constructor(loginServ:LoginService){
+    this.loginServ = loginServ;
+  }
+  login(){
+    if(this.email.length > 0 || this.password.length > 0){
+      this.loginServ.login(this.email, this.password);
+    }
+  }
 }
--- recipeFrontend/src/app/navbar/navbar.component.ts
+++ recipeFrontend/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  router: any;
+  constructor(router:Router) { 
+    this.router=router;
+  }
 
+login(){
+this.router.navigate(['/login'])
+}
+signup(){
+  this.router.navigate(['/signup'])
+}
+
+home(){
+  this.router.navigate(['/home'])
+}
 }
